2fb72770fb2d0740b79a4ce93b762585b793bcfb
Jordan Johnson <jjordan290@gmail.com>
2022-03-31 15:50:15 -0400

    Database Set-up

---
+++ Node/test.js
@@ -0,0 +1,25 @@
+console.log("Juicy")
+const cassandra = require("cassandra-driver")
+
+const client = new cassandra.Client({
+    contactPoints: ["localhost"],
+    localDataCenter: 'datacenter1',
+    keyspace: 'sharkbait'
+  });
+
+//const query = 'INSERT INTO Users (username,displayName,password) VALUES (?,?,?)'
+const query = 'SELECT * FROM Users'
+
+// Inserting items into the database by the user 
+var username = "MScott123"
+var displayName = "MichaelScott"
+var password = "Password123"  
+
+
+//client.execute(query,[username,displayName,password], {prepare: true})
+  client.execute(query).then((result)=>{
+    for (var i = 0; i < result.rows.length; i++) {
+        console.log(result.rows[i].displayname)
+        // For each row it prints whats inside the rows
+    } 
+  })
